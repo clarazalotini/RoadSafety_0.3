3edbbe14d67e3ed01fa29eab9f8c086175911c1c
Maria Clara <clarazalotini@gmail.com>
2023-10-14 20:53:30 -0300

    Adicionar arquivos de projeto.

---
+++ RoadSafety_0.3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoadSafety_0._3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
